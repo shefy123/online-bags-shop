16e6b8d42932abdae892dc7ba0c499157bce75d1
shefy123 <38942966+shefy123@users.noreply.github.com>
2018-11-15 11:56:18 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,16 @@
-// nav
+// FOR NAVBAR
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
-});
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
 
-// slider functionality
-
+//   FOR SLIDER
 let options = {
-  duration: 200
-}
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.slider');
-  var instances = M.Slider.init(elems, options);
-});
-
-// cards functionality
-
-
-function initialize() {
-
-
-  for (let i = 0; i < bagDetails.length; i++) {
-    let bagsEL = document.querySelector('.card-container');
-    bagsEL.innerHTML += ` <div class="card hoverable">
-      <div class="card-image ">
-        <img src="images/${bagDetails[i].img}">
-       
-      </div>
-      <div class="card-content">
-        <p>
-        ${bagDetails[i].discription}
-      </p>
-          <h4>${bagDetails[i].price}</h4>
-      </div>
-      <div class="card-action">
-           
-      <a class="waves-effect waves-light btn brown">BUY NOW</a>
-    </div>
-    </div>
-    `
-
+    duration: 500
   }
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
 
-
-
+ 
